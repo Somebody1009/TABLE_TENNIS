3349b41b88d1c4940e0ca424a0289543f2fd43d7
moonlylight <8042valya@gmail.com>
2025-05-24 19:43:14 +0300

    Реалізовано зміну кольору м’яча залежно від швидкості (білий → помаранчевий → червоний)

--- ball.py
+++ ball.py
@@ -28,3 +28,48 @@ class Ball(Turtle):
         self.goto(0, 0)
         self.move_speed = 0.05
         self.bounce_x()
+        from turtle import Turtle
+        import random
+
+        class Ball(Turtle):
+
+            def __init__(self):
+                super().__init__()
+                self.color("white")
+                self.shape("circle")
+                self.penup()
+                self.x_move = random.choice([-1, 1]) * (2 + random.random() * 2)
+                self.y_move = random.choice([-1, 1]) * (2 + random.random() * 2)
+                self.move_speed = 0.05
+                self.update_color()
+
+            def move(self):
+                new_x = self.xcor() + self.x_move
+                new_y = self.ycor() + self.y_move
+                self.goto(new_x, new_y)
+                self.update_color()
+
+            def bounce_y(self):
+                self.y_move *= -1
+                self.slow_down()
+
+            def bounce_x(self):
+                self.x_move *= -1
+                self.move_speed *= 0.9
+                self.update_color()
+
+            def reset_position(self):
+                self.goto(0, 0)
+                self.move_speed = 0.1
+                self.bounce_x()
+
+            def slow_down(self):
+                self.move_speed *= 1.05
+
+            def update_color(self):
+                if self.move_speed < 0.03:
+                    self.color("red")
+                elif self.move_speed < 0.07:
+                    self.color("orange")
+                else:
+                    self.color("white")
